14186d6e9f5a001f9148189d56aebe3b7cae00b3
1216895863@qq.com <YJHM@windows10.microdone.cn>
2021-01-29 09:15:38 +0800

    第一个测试

---
+++ Test/src/线程/MyCallable.java
@@ -0,0 +1,22 @@
+package Ïß³Ì;
+
+import java.util.concurrent.Callable;
+
+public class MyCallable implements Callable<Integer>{
+	private int number;
+	
+	public MyCallable(int number) {
+		super();
+		this.number = number;
+	}
+
+	@Override
+	public Integer call() throws Exception {
+		int sum=0;
+		for(int i=0;i<number+1;i++) {
+			sum +=i;
+		}
+		return sum;
+	}
+	
+}
